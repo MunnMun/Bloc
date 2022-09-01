29253ffee09dc82cb47fd17ef26458e3ddec8cda
Harsh kaushik <kaushikharsh1619@gmail.com>
2022-09-01 12:59:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:block/bloc/internet_bloc/internet_bloc.dart';
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
+  }
+}
